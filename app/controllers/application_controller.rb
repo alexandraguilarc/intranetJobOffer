2686da292cda3303caf0786099f770ee19ddc6f6
AlexandrAguilarC <alexandra10aguilarc@gmail.com>
2023-09-11 18:40:54 -0300

    Creamos método para verificar el tipo de usuario.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name, images: []])
         devise_parameter_sanitizer.permit(:account_update, keys: [:name, images: []])
     end
+
+    def authorize_request(kind = nil)
+        unless kind.include?(current_user.role)
+        redirect_to posts_path, notice: "You are not authorized to perform this action"
+        end
+    end
 end
